3b6deded7559c4bfa659b658d53975e50ed0ed66
LENOVO <LENOVO@DESKTOP-OL4U103>
2023-12-13 18:49:24 +0530

    temperature converter

---
+++ project1/src/project1/temperatureconverter.java
@@ -0,0 +1,18 @@
+package project1;
+import java.util.Scanner;
+
+public class temperatureconverter {
+	public static void main(String[] args) {
+	Scanner user_input=new Scanner(System.in);
+	float f;
+	double cel;
+	System.out.print("Enter Temperature in Fahrenheit:");
+	f=user_input.nextFloat();
+	cel=(f-32)/1.8;
+	System.out.print("\nFahrenhit is :"+f);
+	System.out.print("\nCelcius is :"+cel);
+    }
+
+
+
+}
